e6d723f6587bf5578588dd43387f66268e34128b
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-06 14:15:22 -0500

    Part 2 basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Group Members: Daniel Mallia and Darren Zhang
+
+// Declarations 
+let part2ParanoidDiv = document.querySelector("#Part2ParanoidDiv");
+
+
+// Part 1 Code
+
+
+// Part 2 Code
+part2ParanoidDiv.addEventListener("mouseenter", function(event) { 
+    alert("Hey, I told you not to hover over me!");
+})
